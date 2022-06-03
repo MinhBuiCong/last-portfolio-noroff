f62e06b42fddc368dbe835802130ce58d224f5db
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-06-03 22:35:50 +0200

    Adding all page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import "./App.css";
+import React from "react";
+import "./App.scss";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import NavBar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Home from "./components/Home/home";
+import Projects from "./components/Projects/projects";
+import ProjectDetail from "./components/Projects/projectDetail";
+import Contact from "./components/Contact/contact";
 
 function App() {
-  return <div className="App">oyoyyoyo</div>;
+  return (
+    <Router>
+      <div>
+        <NavBar className="navbar" expand="lg">
+          <Container className="container">
+            <NavBar.Brand href="/">Portfolio</NavBar.Brand>
+            <NavBar.Toggle aria-controls="basic-navbar-nav" />
+            <NavBar.Collapse id="basic-navbar-nav">
+              <Nav className="nav">
+                <NavLink className="link" to="/">
+                  Home
+                </NavLink>
+                <NavLink className="link" to="/projects">
+                  Projects
+                </NavLink>
+                <NavLink className="link" to="/contact">
+                  Contact
+                </NavLink>
+              </Nav>
+            </NavBar.Collapse>
+          </Container>
+        </NavBar>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects/:id" element={<ProjectDetail />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Projects/projectDetail.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectDetail() {
+  return <div>projectDetail</div>;
+}
+
+export default ProjectDetail;
