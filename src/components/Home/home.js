afa3855f974990487ddc77101360ea8fec375c58
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-06-04 02:03:32 +0200

    update

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "../../App.scss";
+import { AboutData } from "../../constant/aboutData";
 
 function Home() {
+  const { id, title, image, description, gitLink, facebookLink, igLink } =
+    AboutData;
   return (
-    <main className="drinks">
-      <h1>home</h1>
-    </main>
+    <div className="about">
+      <h1>{title}</h1>
+      <img src={image} alt={id} />
+      <p>{description}</p>
+      <a href={gitLink}>GitHub</a>
+      <a href={facebookLink}>facebook</a>
+      <a href={igLink}>instagram</a>
+    </div>
   );
 }
 export default Home;
---
+++ src/constant/aboutData.js
@@ -0,0 +1,11 @@
+import me from "../image/me.JPEG";
+
+export const AboutData = {
+  id: 0,
+  title: "About",
+  image: me,
+  description: "about me description",
+  gitLink: "https://github.com/MinhBuiCong",
+  facebookLink: "https://www.facebook.com/MiBuCo/",
+  igLink: "https://www.instagram.com/minhb94/",
+};
