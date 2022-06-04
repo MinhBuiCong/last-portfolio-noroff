a50b70f882c1e496e5879f9399e6a7a91ae36e84
MinhBuiCong <62849019+MinhBuiCong@users.noreply.github.com>
2022-06-04 03:14:47 +0200

    update home

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { AboutData } from "../../constant/aboutData";
+import { FaLinkedin, FaInstagram, FaFacebook, FaGithub } from "react-icons/fa";
 
 function Home() {
   const {
-    id,
     title,
     image,
     intro,
@@ -15,16 +15,31 @@ function Home() {
     linkedInLink,
   } = AboutData;
   return (
-    <div className="about">
-      <h1>{title}</h1>
-      <img src={image} alt={id} />
-      <p>{intro}</p>
-      <p>{main}</p>
-      <p>{end}</p>
-      <a href={gitLink}>GitHub</a>
-      <a href={facebookLink}>Facebook</a>
-      <a href={igLink}>Instagram</a>
-      <a href={linkedInLink}>LinkedIn</a>
+    <div className="about-container">
+      <h1 className="about-title">{title}</h1>
+      <div
+        className="about-img"
+        style={{ backgroundImage: `url(${image})` }}
+      ></div>
+      <div className="about-text-container">
+        <p className="about-text">{intro}</p>
+        <p className="about-text">{main}</p>
+        <p className="about-text">{end}</p>
+      </div>
+      <div className="about-link-container">
+        <a className="about-link" href={gitLink}>
+          <FaGithub />
+        </a>
+        <a className="about-link" href={facebookLink}>
+          <FaFacebook />
+        </a>
+        <a className="about-link" href={igLink}>
+          <FaInstagram />
+        </a>
+        <a className="about-link" href={linkedInLink}>
+          <FaLinkedin />
+        </a>
+      </div>
     </div>
   );
 }
